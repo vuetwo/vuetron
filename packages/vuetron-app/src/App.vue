61c66b59300ededc86b4d1b470e457194e7e79e8
louis <louisrouaze@gmail.com>
2017-11-02 12:18:12 -0700

    components in progress

--- packages/vuetron-app/src/App.vue
+++ packages/vuetron-app/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-
+  <div id="sidebar-wrapper">
+      <ul><button v-on:click="timeline">Timeline</button></ul>
+      <ul><button v-on:click="selectedPage">Subscription</button></ul>
+      <ul><button v-on:click="stateView">Stateview</button></ul>
   </div>
 </template>
 
 <script>
+import Timeline from './components/Timeline.vue';
+import Subscription from './components/Subscription.vue';
+import Stateview from './components/Stateview.vue';
 
 export default {
   name: 'app',
   components: {
-    
+    Timeline,
+    Subscription,
+    Stateview
   },
 };
 </script>
--- packages/vuetron-app/src/main.js
+++ packages/vuetron-app/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import App from './App';
+import App from './App.vue';
 
 new Vue({
-  el: '#vuetron',
-  template: '<App/>',
-  components: { App },
+  el: '#app',
+  render: h => h(App)
 });
+
