0d9aa596e2a1c0c92d9aeac3994fb00bfde1cb1a
Samantha Salley <samanthasalley@gmail.com>
2017-11-02 17:25:26 -0700

    update plugin to listen for state changes from server and replace existing app state

--- packages/vuetron-vuex/index.js
+++ packages/vuetron-vuex/index.js
@@ -8,6 +8,15 @@ const VuetronVuex = function (port = 9090) {
     // emit initial state to server
     socket.emit('clientStateInit', store.state);
 
+    // listen for state changes from Vuetron and update
+    //  app state accordingly
+    socket.on('updateClientState', function(newState){
+      // parse the stringified new state passed from Vuetron
+      let parsedState = JSON.parse(newState);
+      // replace app's current state with the parsed Vuetron state
+      store.replaceState(parsedState);
+    });
+
     // subscribe to store mutations
     store.subscribe((mutation, state) => {
       // on mutation, emit update event to server
