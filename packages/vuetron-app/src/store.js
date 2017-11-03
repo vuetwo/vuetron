cf0988bccb8b1528df5a3918ba1900904b5a3f8d
Samantha Salley <samanthasalley@gmail.com>
2017-11-03 12:08:54 -0700

    customize vuetron app vuex plugin with vuetron-specific listeners and emitters

--- packages/vuetron-app/src/store.js
+++ packages/vuetron-app/src/store.js
@@ -1,8 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-// Import Vuetron-Vuex plugin to initialize connection to sockets
-import VuetronVuex from 'vuetron-vuex';
+const io = require('socket.io-client');
+
+// setup plugin to connect vuex store to server sockets
+const VuetronVuex = function (port = 9090) {
+  return store => {
+    // initialize socket connection
+    const socket = io('http://localhost:' + port);
+
+    // emit initial state to server
+    socket.emit('vuetronStateUpdate', store.state);
+
+    // listen for state changes from client and update
+    //  vuetron's client state store accordingly along
+    //  with mutation log
+    socket.on('stateUpdate', function(mutation, newState){
+      // add mutation to mutation log
+      // update client's current state to newState
+    });
+
+    // subscribe to store mutations
+    store.subscribe((mutation, state) => {
+      // on mutation, check if mutation is updating
+      // client's state
+        // if so, emit update event to server
+          // socket.emit('vuetronStateUpdate', state);
+    })
+  }
+}
 
 Vue.use(Vuex);
 
