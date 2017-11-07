15f57c4c4ccf944fbc9191616b6d76ed60737051
Samantha Salley <samanthasalley@gmail.com>
2017-11-06 21:18:07 -0800

    implement VueObjectView component library to render client state as interactive Javascript object.

--- packages/vuetron-app/src/components/State.vue
+++ packages/vuetron-app/src/components/State.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
    <h1>{{ msg }}</h1>
-   <h1>{{ clientState }}</h1>
+   <div v-if="show && clientState">
+    <VueObjectView :value="clientState" />
+   </div>
+   <div>{{ clientState }}</div>
   </div>
 </template>
  
  <script>
- export default {
-   data() {
-     return { 
-       msg: 'Hello Sam'
-     }
-   },
-   computed: {
-     clientState() {
-       return this.$store.state.clientState;
-     }
-   }
- }
- </script>
+import VueObjectView from "vue-object-view";
+
+export default {
+  data() {
+    return {
+      msg: "Hello Sam",
+      show: true // required for properly re-rendering object view on change
+    };
+  },
+  computed: {
+    clientState() {
+      this.show = false; 
+      this.$nextTick(() => {
+        this.show = true;
+      });
+      return this.$store.state.clientState;
+    }
+  },
+  components: {
+    VueObjectView
+  }
+};
+</script>
