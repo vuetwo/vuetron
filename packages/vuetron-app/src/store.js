c2d5661745e59d181ce0e16365759bcee254b037
Samantha Salley <samanthasalley@gmail.com>
2017-11-07 15:20:55 -0800

    clean-up socket functions, add real events for connection and state initialization, add method to toggle data show on specific events

--- packages/vuetron-app/src/store.js
+++ packages/vuetron-app/src/store.js
@@ -8,11 +8,26 @@ const VuetronVuex = function (port = 9090) {
   return store => {
     // initialize socket connection
     const socket = io('http://localhost:' + port);
+    // register event noting connection to sockets (client app)
+    let initEvent = {
+      title: 'CONNECTED TO APP',
+      display: {
+        msg: 'Successfully connected Vuetron to client'
+      }
+    };
+    store.commit('addNewEvent', initEvent);
 
     // request current client state on socket connection
     socket.emit('requestClientState');
 
     socket.on('setInitState', function(state) {
+      let event = {
+        title: 'STATE INITIALIZED',
+        display: state
+      };
+      // register event noting receipt of initial client state
+      store.commit('addNewEvent', event);
+      // initialize client state value
       store.commit('updateClientState', state);
     });
 
@@ -20,55 +35,54 @@ const VuetronVuex = function (port = 9090) {
     //  vuetron's client state store accordingly along
     //  with mutation log
     socket.on('stateUpdate', function(mutation, newState){
-      console.log('got state update', mutation, newState);
-      // add mutation to mutation log
-      // store.state.mutations.unshift(mutation);
       let updatedState = {
         title: 'STATE CHANGE',
-        mutation: mutation,
-        newState: JSON.stringify(newState),
-        show: false
+        display: {
+          mutation: mutation,
+          newState: JSON.stringify(newState),
+        }
       };
+      // register event for state change
       store.commit('addNewEvent', updatedState);
       // update client's current state to newState
       store.commit('updateClientState', newState);
     });
 
-    socket.on('sendEvent', function(event){
-      console.log('got new event', event);
-      // store.state.events.unshift(event);
-    });
+    // socket.on('sendEvent', function(event){
+    //   console.log('got new event', event);
+    //   // store.state.events.unshift(event);
+    // });
 
 
-    //TESTING WITH DUMMY DATA:
+    // //TESTING WITH DUMMY DATA:
 
-    //get state change:
-    socket.on('sendMutation', function(mutation, newState){
-      let updatedStateItem = {
-        title: 'STATE CHANGE',
-        display: JSON.stringify(mutation),
-        newState: JSON.stringify(newState)
-      };
-      store.commit('addNewEvent', updatedStateItem);
-    });
+    // //get state change:
+    // socket.on('sendMutation', function(mutation, newState){
+    //   let updatedStateItem = {
+    //     title: 'STATE CHANGE',
+    //     display: JSON.stringify(mutation),
+    //     newState: JSON.stringify(newState)
+    //   };
+    //   store.commit('addNewEvent', updatedStateItem);
+    // });
 
-    //get client event:
-    socket.on('sendClientEvent', function(type, payload){
-      let clientStateItem = {
-        title: 'ACTION',
-        display: JSON.stringify(payload),                
-        type: type
-      }
-      store.commit('addNewEvent', clientStateItem);
-    });
+    // //get client event:
+    // socket.on('sendClientEvent', function(type, payload){
+    //   let clientStateItem = {
+    //     title: 'ACTION',
+    //     display: JSON.stringify(payload),                
+    //     type: type
+    //   }
+    //   store.commit('addNewEvent', clientStateItem);
+    // });
 
-    // subscribe to store mutations
-    store.subscribe((mutation, state) => {
-      // on mutation, check if mutation is updating
-      // client's state
-        // if so, emit update event to server
-          // socket.emit('vuetronStateUpdate', state);
-    })
+    // // subscribe to store mutations
+    // store.subscribe((mutation, state) => {
+    //   // on mutation, check if mutation is updating
+    //   // client's state
+    //     // if so, emit update event to server
+    //       // socket.emit('vuetronStateUpdate', state);
+    // })
   }
 }
 
@@ -76,14 +90,16 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        clientState: null,  //state from client
-        events: [{title:'CONNECTED TO APP'}, {title:'STATE INITIALIZED'}]
+        clientState: {},  //state from client
+        events: []
     },
     mutations: {
       updateClientState (state, newClientState) {
         state.clientState = newClientState;
       },
       addNewEvent(state, newEvent) {
+        if(!newEvent.title || !newEvent.display) throw new Error('invalid event data');
+        if(!newEvent.show) newEvent.show = false;
         state.events.unshift(newEvent);
       },
       toggleEventShow(state, evIdx){
