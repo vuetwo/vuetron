f63f17769ca2457bf1f394096839fcb60e33f373
brandondanh <brandondanh+github@gmail.com>
2017-11-07 17:47:41 -0800

    editted code to match semi-standard style, still need to double check vuetron-server and electron.js for excessive code

--- packages/vuetron-app/src/main.js
+++ packages/vuetron-app/src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import { store } from './store';
 
+/* eslint-disable no-new */
 new Vue({
   el: '#vuetron',
   template: '<App/>',
   store,
   components: { App }
 });
-
+/* eslint-disable-line no-new */
