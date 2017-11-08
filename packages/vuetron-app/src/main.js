12cf26fed84b42542028ca98c76fb39a06ba4da0
Samantha Salley <samanthasalley@gmail.com>
2017-11-08 10:58:07 -0800

    require and import and configure Bootstrap-Vue for app styling

--- packages/vuetron-app/src/main.js
+++ packages/vuetron-app/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import { store } from './store';
 
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#vuetron',
