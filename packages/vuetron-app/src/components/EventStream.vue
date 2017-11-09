0db40c297118e8dc8b02ef792a9274b64404e343
Samantha Salley <samanthasalley@gmail.com>
2017-11-09 12:02:17 -0800

    event stream bootstrapping in progress

--- packages/vuetron-app/src/components/EventStream.vue
+++ packages/vuetron-app/src/components/EventStream.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
-      <h1 @click="() => {emitEventToggle(index)}">{{ event.title }}</h1>
-      <div v-show="event.show">
-        <p>{{ event.display }}</p>
-      </div>
-    </div>
+    <b-row>
+      <b-col cols="11"></b-col>
+      <b-col cols="1">
+        
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12">
+        <h1>{{ msg }}</h1>
+        <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
+          <h1 @click="() => {emitEventToggle(index)}">{{ event.title }}</h1>
+          <div v-show="event.show">
+            <p>{{ event.display }}</p>
+          </div>
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
  
