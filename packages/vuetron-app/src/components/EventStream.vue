fbe14b53d1cdcb47290c3cdaa46e89aa611224d9
Samantha Salley <samanthasalley@gmail.com>
2017-11-09 11:57:27 -0800

    fix Event Stream template formatting

--- packages/vuetron-app/src/components/EventStream.vue
+++ packages/vuetron-app/src/components/EventStream.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-      <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
-        <h1 @click="() => {emitEventToggle(index)}">{{ event.title }}</h1>
-        <div v-show="event.show">
-          <p>{{ event.display }}</p>
-        </div>
+    <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
+      <h1 @click="() => {emitEventToggle(index)}">{{ event.title }}</h1>
+      <div v-show="event.show">
+        <p>{{ event.display }}</p>
       </div>
+    </div>
   </div>
 </template>
  
