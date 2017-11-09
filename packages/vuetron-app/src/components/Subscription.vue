53952bccd1230c7901b0220212bbd91b816a9706
Samantha Salley <samanthasalley@gmail.com>
2017-11-08 16:47:41 -0800

    add subscription display, add input to subscribe to new path, add method to commit new subscription to store state, add computed method to get subscriptions from store state

--- packages/vuetron-app/src/components/Subscription.vue
+++ packages/vuetron-app/src/components/Subscription.vue
@@ -1,18 +1,44 @@
 <template>
   <div>
-    <!-- <h1>{{ msg }}</h1> -->
+    <div v-if="show && subscriptions">
+      <ul>
+        <li v-for="(value, key, index) in subscriptions" v-bind:key="index">
+          <h3>{{ key }}</h3>
+          <VueObjectView :value="value" />
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p><strong>Add Subscription</strong></p>
+      <input type="text" placeholder="subscription path" v-model="newSub"/>
+      <b-button @click="addSub" variant="success">Subscribe</b-button>
+    </div>
   </div>
 </template>
  
  <script>
- export default {
-  //  data() {
-  //    return {
-  //      msg: 'Hello Kelly'
-  //   }
-  //  },
-  //  methods: {
-
-  //  }
- }
- </script>
+ import VueObjectView from "vue-object-view";
+export default {
+  data() {
+    return {
+      newSub: null,
+      show: true
+    };
+  },
+  computed: {
+    subscriptions() {
+      this.show = false;
+      this.$nextTick(() => this.show = true);
+      return this.$store.state.subscriptions;
+    }
+  },
+  methods: {
+    addSub() {
+      this.$store.commit('addSubscription', this.newSub);
+    }
+  },
+  components: {
+    VueObjectView,
+  },
+};
+</script>
