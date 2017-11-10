f7842fca7e84b69e31581b7b6480ee38860086db
Samantha Salley <samanthasalley@gmail.com>
2017-11-09 17:44:43 -0800

    add filter logic and functionality for event stream filtering

--- packages/vuetron-app/src/components/EventStream.vue
+++ packages/vuetron-app/src/components/EventStream.vue
@@ -1,101 +1,99 @@
 
-<!-- <template id="eventStreamTemplate">
+<template id="eventStreamTemplate">
   <div>
-    <b-row>
-      <b-col cols="11"></b-col>
-      <b-col cols="1">
-        
-      </b-col>
-    </b-row>
+    <nav class="navbar sticky-top navbar-light bg-faded">
+      <h1 id="headerStyle" class="navbar-brand mb-0">Event Stream</h1>
+      <b-dropdown no-caret variant="transparent"
+        v-b-popover.hover.auto.left="'Filter events'">
+        <template slot="button-content">
+          <icon name="filter"></icon>
+        </template>
+        <b-dropdown-header>Event Types</b-dropdown-header>
+        <b-container>
+          <b-form-checkbox-group v-model="selected" 
+            :options="checkEventTypes" stacked>
+          </b-form-checkbox-group>
+        </b-container>
+      </b-dropdown>
+    </nav>
     <b-row>
       <b-col cols="12">
-        <h1>{{ msg }}</h1>
-        <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
-          <h1 @click="() => {emitEventToggle(index)}">{{ event.title }}</h1>
-          <div v-show="event.show">
-            <p>{{ event.display }}</p>
+        <div class="row" v-for="(event, index) in filteredEvents" 
+          v-bind:event="event" v-bind:key="index">
+          <b-btn class="eventBtn" @click="() => {emitEventToggle(index)}">{{ event.title }}</b-btn>
+          <div class="eventCardWrapper" v-show="event.show">
+            <b-card class="eventCard">
+              <h5>{{ event.title }}</h5>
+              <div>{{ event.display }}</div>
+            </b-card>
           </div>
         </div>
       </b-col>
     </b-row>
-    <h1>{{ msg }}</h1>
-      <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
-          <b-btn class="eventBtn" @click="() => {emitEventToggle(index)}">{{ event.title }}</b-btn>
-          <div class ="eventCardWrapper"v-show="event.show">
-            <b-card class="eventCard">{{ event.display }}</b-card>
-         </div>
-      </div>
-       </div>
   </div>
-</template> -->
-<template>
-
-    <!-- <b-row class="row2" cols="8" md="9" lg="10">
-        <header id="eventStreamHeader">Event Stream</header>
-      </b-row> -->
-    
-    <b-container class="padding-0">
-        <nav class="navbar sticky-top navbar-light bg-faded">
-            <h1 id="headerStyle" class="navbar-brand mb-0">Event Stream</h1>
-          </nav>
-        <div class="row" v-for="(event, index) in events" v-bind:event="event" v-bind:key="index">
-            <b-btn class="eventBtn" @click="() => {emitEventToggle(index)}">{{ event.title }}</b-btn>
-            <div class="eventCardWrapper"v-show="event.show">
-              <b-card class="eventCard">
-                <h5>{{ event.title }}</h5>
-                {{ event.display }}
-              </b-card>
-           </div>
-        </div>
-         </div>
-    </b-container>
-  </template>
+</template>
   
- <script>
- export default {
-   data() {
-        return {
-          msg: 'Hello Louis'
-        }
+<script>
+  export default {
+    data() {
+      return {
+        eventTypes: new Set(),
+        selected: []
+      };
     },
-   computed: {
-     events () {
-      return this.$store.state.events;
-     }
-   },
-   methods: {
-     emitEventToggle(evIdx) {
-      this.$store.commit("toggleEventShow", evIdx);
+    computed: {
+      filteredEvents() {
+        return this.$store.state.events.filter(ev => this.selected.indexOf(ev.title.split(' ').join('_')) !== -1);
+      },
+      checkEventTypes() {
+        this.$store.state.events.forEach(ev => {
+          let name = ev.title.split(' ').join('_')
+          if(!this.eventTypes.has(name)) this.selected.push(name);
+          this.eventTypes.add(name);
+        });
+        return Array.from(this.eventTypes);
+      }
+    },
+    methods: {
+      emitEventToggle(evIdx) {
+        this.$store.commit('toggleEventShow', evIdx);
+      }
     }
-   }
+  };
+</script>
+
+<style>
+  .btn-transparent {
+    background-color: transparent;
   }
- </script>
- <style scoped>
-   /* #eventStreamHeader {
-    /* position: fixed;
-    top: 0;
-    width: 100%;
-    height: 60px;
-    font-size: 30px;
-    text-align: center;
-    line-height: 60px;    /* center text vertically */
-    /* color: #0B9BD7;  */
-    /* background-color: #2F4B5C; */
-   /* } */
+  .btn-transparent:focus {
+    box-shadow: none;
+  }
+</style>
+
+<style scoped>
+  .fa-icon {
+    color: #0b9bd7;
+  }
+  .fa-icon:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+
   #headerStyle {
-    color: #0B9BD7;
+    color: #0b9bd7;
   }
   .eventBtn {
     margin-left: 3.5%;
     margin-right: 3.5%;
-    margin-top: -1px;     /* removes overlapping borders */ 
-    margin-bottom: -1px;  /* removes overlapping borders */
+    margin-top: -1px; /* removes overlapping borders */
+    margin-bottom: -1px; /* removes overlapping borders */
     width: 100%;
     padding-top: 10px;
     padding-bottom: 10px;
-    color: #2F4B5C;
-    background-color: #31B689;
-    border: 1px solid #2F4B5C;
+    color: #2f4b5c;
+    background-color: #31b689;
+    border: 1px solid #2f4b5c;
     border-radius: 0px;
     border-left: none;
     border-right: none;
@@ -106,9 +104,9 @@
   .eventCard {
     width: 100%;
     border-radius: 0px;
-    border-bottom: 1px solid #2F4B5C;
-    color: #2F4B5C;
-    background-color: #EBEBEB;
+    border-bottom: 1px solid #2f4b5c;
+    color: #2f4b5c;
+    background-color: #ebebeb;
   }
   .eventCardWrapper {
     margin-left: 3.5%;
@@ -116,9 +114,9 @@
     width: 100%;
   }
   .padding-0 {
-    padding-right:0;
-    padding-left:0;
+    padding-right: 0;
+    padding-left: 0;
   }
- </style>
+</style>
 
  
