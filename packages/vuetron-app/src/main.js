c97f5fba9eceaa56519f00c0210bba9f5f80b720
Samantha Salley <samanthasalley@gmail.com>
2017-11-09 17:44:17 -0800

    add vue-awesome package for icons and import into project globally

--- packages/vuetron-app/src/main.js
+++ packages/vuetron-app/src/main.js
@@ -6,27 +6,26 @@ import Subscription from './components/Subscription.vue';
 import EventStream from './components/EventStream.vue';
 import State from './components/State.vue';
 
-// import BootstrapVue from 'bootstrap-vue';
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-// Vue.use(BootstrapVue);
+import Icon from 'vue-awesome/components/Icon.vue';
+import 'vue-awesome/icons';
+
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.component('icon', Icon);
 
 const routes = [
   { path: '/subscription', component: Subscription },
   { path: '/state', component: State },
   { path: '/eventstream', component: EventStream}
-]
+];
+
 const router = new VueRouter({
   routes
-})
-
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-Vue.use(BootstrapVue);
+});
 
 /* eslint-disable no-new */
 new Vue({
