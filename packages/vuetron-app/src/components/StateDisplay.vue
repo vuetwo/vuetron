65cfcf164a5e62e170bd0887e689fcb671caf9ce
Samantha Salley <samanthasalley@gmail.com>
2017-11-18 09:44:39 -0800

    Add custom display for State Initialized events

---
+++ packages/vuetron-app/src/components/StateDisplay.vue
@@ -0,0 +1,36 @@
+
+<template id="stateDisplayTemplate">
+  <VueObjectView :value="processInfo" />
+</template>
+  
+<script>
+  import VueObjectView from 'vue-object-view';
+  export default {
+    name: 'StateDisplay',
+    props: ['info'],
+    data() {
+      return {
+        processedInfo: null
+      };
+    },
+    computed: {
+      processInfo() {
+        this.processedInfo = Object.assign({}, this.info);
+        return this.processedInfo;
+      }
+    },
+    components: {
+      VueObjectView
+    }
+  };
+</script>
+
+<style>
+  
+</style>
+
+<style scoped>
+  
+</style>
+
+ 
