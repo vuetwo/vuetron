1dee2f5fad1f856bb5226c3046f22fd16687ca05
Samantha Salley <samanthasalley@gmail.com>
2017-11-26 18:05:55 -0800

    add check for initial state.  If non, display waiting message

--- packages/vuetron-app/src/components/State.vue
+++ packages/vuetron-app/src/components/State.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
+  <div class="full-height">
     <navbar title="State" />
-    <b-container v-if="show && clientState" fluid>
+    <b-container v-if="notNull && show && clientState" fluid>
       <vue-object-view :value="clientState" /> 
+    </b-container>
+    <b-container v-if="!notNull" class="full-height" fluid>
+      <b-row class="full-height-middle">
+        <a class="no-state-logo text-center" v-b-modal.addSubModal>
+          <div class="no-state-icon">
+            <icon name="frown-o" scale="6" />
+          </div>
+          <div class="no-state-text">
+            <h2>No State Found</h2>
+            <p>Waiting for app connection...</p>
+          </div>
+        </a>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -23,12 +36,8 @@
         });
         return this.$store.state.clientState;
       },
-      instaPics() {
-        console.log("CHECK ARR", this.$store.state.instaPics);
-        return this.$store.state.instaPics;
-      },
-      events () {
-        return this.$store.state.events;
+      notNull() {
+        return Object.keys(this.$store.state.clientState).length > 0;
       }
     },
     components: {
@@ -37,4 +46,36 @@
 };
 </script>
 <style scoped>
+  .no-state-logo {
+    height: 300px;
+    width: 300px;
+    margin: 15px auto;
+    color: #001453;
+    background-color: rgba(5, 248, 180, 0.31);
+    border-radius: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-decoration: none;
+  }
+
+  .no-state-icon {
+    text-align: center;
+  }
+
+  .no-state-text {
+    text-align: center;
+    padding: 20px;
+  }
+
+  .full-height {
+    height: 100%;
+  }
+
+  .full-height-middle {
+    height: 65%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 </style>
