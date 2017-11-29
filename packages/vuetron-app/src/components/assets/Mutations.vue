8bf77f54f49243942a27fa3f70d6a1e9b9fb59d6
Samantha Salley <samanthasalley@gmail.com>
2017-11-29 11:00:29 -0800

    add AddSubBtn to path item on Mutations

--- packages/vuetron-app/src/components/assets/Mutations.vue
+++ packages/vuetron-app/src/components/assets/Mutations.vue
@@ -8,12 +8,14 @@
         :aria-expanded="item.show ? 'true' : 'false'">
         <icon :name="item.show ? 'caret-down' : 'caret-right'" class="caret-icon" />
       </b-btn>
-      <span> {{ index + 1 }}</span>
+      <span> {{ index + 1 }} - {{ item.props.path }}</span>
       <b-collapse class="mt-2" :id="`item-${index}`" v-model="item.show">
         <b-card>
           <b-list-group>
             <b-list-group-item v-for="(value, key, i) in item.props" :key="i">
               <strong>{{ key }}</strong>: {{ value }}
+              <add-sub-btn v-if="key === 'path'" 
+                v-on:click.native="() => $store.commit('addSubscription', value)" />
             </b-list-group-item>
           </b-list-group>
         </b-card>
@@ -23,6 +25,7 @@
 </template>
   
 <script>
+  import AddSubBtn from './AddSubBtn.vue';
   const checkType = (char) => {
     switch (char) {
       case 'E':
@@ -52,26 +55,31 @@
     },
     computed: {
       processChanges() {
-        this.processedChanges = this.changes.map(change => {
-          const newObj = {};
-          newObj.show = false;
-          newObj.props = {};
-          // set change path (str)
-          newObj.props.path = change.path.join('.');
-          // set change type and changed data
-          if (checkType(change.kind)) {
-            newObj.props.type = checkType(change.kind);
-            if (change.lhs) newObj.props.before = change.lhs;
-            if (change.rhs) newObj.props.after = change.rhs;
-          } else if (change.kind === 'A') {
-            newObj.props.type = checkType(change.item.kind);
-            if (change.item.lhs) newObj.props.before = change.item.lhs;
-            if (change.item.rhs) newObj.props.after = change.item.rhs;
-          }
-          return newObj;
-        });
+        if (this.changes) {
+          this.processedChanges = this.changes.map(change => {
+            const newObj = {};
+            newObj.show = false;
+            newObj.props = {};
+            // set change path (str)
+            newObj.props.path = change.path.join('.');
+            // set change type and changed data
+            if (checkType(change.kind)) {
+              newObj.props.type = checkType(change.kind);
+              if (change.lhs) newObj.props.before = change.lhs;
+              if (change.rhs) newObj.props.after = change.rhs;
+            } else if (change.kind === 'A') {
+              newObj.props.type = checkType(change.item.kind);
+              if (change.item.lhs) newObj.props.before = change.item.lhs;
+              if (change.item.rhs) newObj.props.after = change.item.rhs;
+            }
+            return newObj;
+          });
+        }
         return this.processedChanges;
       }
+    },
+    components: {
+      'add-sub-btn': AddSubBtn
     }
   };
 </script>
